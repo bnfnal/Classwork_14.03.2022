dffa2d1d2475185da33f3125bfe9c1f0daf2e1a3
Nal Bilalova <59770118+bnfnal@users.noreply.github.com>
2022-03-14 12:54:30 +0300

    Update Main.java
    Task 2: Count Odd Numbers below n

--- Main.java
+++ Main.java
@@ -2,13 +2,23 @@ import java.util.Scanner;
 
 // Task 1: Points of Reflection 
 
-public class Reflections {
-    
+public class PointsOfReflection 
+{
   public static int[] reflectPoint(int[] p, int[] q) {
     int[] p1 = new int[p.length];
     for (int i = 0; i<p.length; i++){
             p1[i] = 2*q[i] - p[i];
         }
     return p1;
+  }
+}
+
+// Task 2: Count Odd Numbers below n
+
+public class CountOddNumbers {
+  public static int oddCount(int n)
+  {
+    int count = n/2;
+    return count;
   }
 }
