9377cb51cdad1277c12a9898cad4951207a10249
Nal Bilalova <59770118+bnfnal@users.noreply.github.com>
2022-03-27 18:28:06 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,43 +1,50 @@
 import java.util.Scanner;
 
-// Task 1: Points of Reflection 
+// Task 1: If you can't sleep, just count sheep!! 
 
-public class PointsOfReflection 
+public class CountingSheeps 
 {
-  public static int[] reflectPoint(int[] p, int[] q) {
-    int[] p1 = new int[p.length];
-    for (int i = 0; i<p.length; i++){
-            p1[i] = 2*q[i] - p[i];
+  public static String countingSheep(int n) {
+    String res = "";
+    for (int i = 1; i<n+1; i++){
+            res += i + " sheep...";
         }
-    return p1;
+    return res;
   }
 }
 
-// Task 2: Count Odd Numbers below n
+// Task 2: Jaden Casing Strings
 
-public class CountOddNumbers {
-  public static int oddCount(int n)
+public class JadenCase 
+{
+  public String toJadenCase(String phrase) 
   {
-    int count = n/2;
-    return count;
+    if (phrase == "" || phrase == null) return null;
+    String[] words = phrase.split(" ");
+    String res = "";
+		for (int i = 0; i<words.length; i++)
+    {
+      res += words[i].substring(0, 1).toUpperCase() + words[i].substring(1, words[i].length());
+      if (i<words.length-1) res += " ";
+    }
+    return res;
   }
 }
 
-// Task 3: Sentence Smash
+// Task 3: You're a square!
 
 import java.util.Arrays;
 
-public class SmashWords {
-
-	public static String smash(String... words) {
-    int n = words.length;
-    String res = "";
-    if (n == 0) return res;
-    for (int i = 0; i<n - 1; i++){
-      res += words[i];
-      res += ' ';
+public class Square 
+{    
+    public static boolean isSquare(int n) 
+    { 
+      if (n < 0) return false;
+      if (n == 0) return true;
+      for (int i = 1; i < (int)Math.sqrt(n) + 1; i++)
+      {
+        if (n == i*i) return true;
+      }
+        return false;
     }
-    res += words[n-1];
-    return res;
-  }
 }
